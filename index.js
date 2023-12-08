be5bc7a63390104ca60379bf490e4169e03586fa
Mariano Garmendia <marianogarmendia77@gmail.com>
2023-12-08 14:25:03 -0300

    finalizado footer menu-hidden primer etapa

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-function main() {}
+const openMenu = document.querySelector("#open-menu");
+const closeMenu = document.querySelector("#close-menu");
+const capaMenu = document.querySelector(".menu-flotante-mobile");
+const enlaces = document.querySelectorAll("#enlaces-menu-escondido");
+
+function main() {
+  openMenu.addEventListener("click", (e) => {
+    capaMenu.classList.add("visible");
+  });
+  closeMenu.addEventListener("click", () => {
+    capaMenu.classList.remove("visible");
+  });
+  enlaces.forEach((enlace) => {
+    enlace.addEventListener("click", () => {
+      capaMenu.classList.remove("visible");
+    });
+  });
+}
 
 main();
